8138759b2e101edfe81b891386995b0a64aea8de
j-x-mochiuti-x <joaomochi@gmail.com>
2026-02-28 16:43:56 -0300

    criando ciclo de vida para analise CRM

--- src/analytics/dau.sql
+++ src/analytics/dau.sql
@@ -1,1 +1,11 @@
-SELECT * FROM transacoes LIMIT 10;
+WITH tb_daily as (
+
+    SELECT substr(DtCriacao,0, 11) AS DtDia,
+        COUNT(DISTINCT IdCliente) AS DAU
+    FROM transacoes
+    GROUP BY 1
+    ORDER BY DtDia
+    LIMIT 10
+)
+
+SELECT * FROM tb_daily
---
+++ src/analytics/life_cicle.sql
@@ -0,0 +1,50 @@
+WITH tb_daily AS (
+    SELECT DISTINCT
+    IdCliente,
+    substr(DtCriacao,1, 10) AS DtDia
+    FROM transacoes
+    WHERE DtCriacao < '2026-02-28'
+),
+
+tb_idade AS (
+SELECT IdCliente,
+    --MIN(DtDia) AS DtPrimeiraEntrada,
+    cast(MAX(julianday('now') - julianday(Dtdia)) AS int) AS DiasDesdePrimeiraEntrada,
+    --MAX(DtDia) AS DtUltimaEntrada,
+    cast(MIN(julianday('now') - julianday(Dtdia)) AS int) AS DiasDesdeUltimaEntrada
+FROM tb_daily
+GROUP BY IdCliente
+),
+
+tb_rn AS (
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY IdCliente ORDER BY DtDia DESC) AS rnDia
+    FROM tb_daily
+),
+
+tb_penultimaAtivacao AS (
+    SELECT *,
+    CAST(julianday('now') - julianday(Dtdia) AS INT) AS DiasDesdePenultimaEntrada
+    FROM tb_rn
+    WHERE rnDia = 2
+),
+
+tblife_cicle AS (
+SELECT t1.*,
+        t2.DiasDesdePenultimaEntrada,
+        CASE
+            WHEN DiasDesdePrimeiraEntrada <= 7 THEN '01 CURIOSO'
+            WHEN DiasDesdeUltimaEntrada <= 7 AND DiasDesdePenultimaEntrada - DiasDesdeUltimaEntrada <= 14 THEN '02 LEAL'
+            WHEN DiasDesdeUltimaEntrada BETWEEN 8 AND 14 THEN '03 TURISTA'
+            WHEN DiasDesdeUltimaEntrada BETWEEN 15 AND 28 THEN '04 DESENCATANDO'
+            WHEN DiasDesdeUltimaEntrada > 28 THEN '05 ZUMBI'
+            WHEN DiasDesdeUltimaEntrada <= 7 AND DiasDesdePenultimaEntrada - DiasDesdeUltimaEntrada BETWEEN 15 AND 27 THEN '06 REATIVADO'
+            WHEN DiasDesdeUltimaEntrada <= 7 AND DiasDesdePenultimaEntrada - DiasDesdeUltimaEntrada > 27 THEN '07 REBORN'
+        END AS life_cicle
+ FROM tb_idade AS t1
+LEFT JOIN tb_penultimaAtivacao AS t2
+ON t1.IdCliente = t2.IdCliente
+)
+
+SELECT date('2026-02-28', '-1 day') AS DtRef, *
+FROM tblife_cicle
