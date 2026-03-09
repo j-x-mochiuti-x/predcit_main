dae89bd87b9f48e764bbe95371757876e433fcfa
j-x-mochiuti-x <joaomochi@gmail.com>
2026-03-09 12:16:04 -0300

    Analise de usuarios de frequencia em dias

---
+++ src/analytics/fs_transacional.sql
@@ -0,0 +1,15 @@
+WITH tb_transacoes AS (
+    SELECT *,
+        substr(DtCriacao,0,11) AS dtDia
+    FROM transacoes
+    WHERE DtCriacao < '2026-02-26'
+)
+
+SELECT IdCliente,
+    count(DISTINCT dtDia) AS qtdeAtivacaoDia,
+    count(DISTINCT CASE WHEN dtDia >= date('2026-02-26', '-7 day') THEN dtDia END) AS qtdeAtivacao7,
+    count(DISTINCT CASE WHEN dtDia >= date('2026-02-26', '-14 day') THEN dtDia END) AS qtdeAtivacao14,
+    count(DISTINCT CASE WHEN dtDia >= date('2026-02-26', '-28 day') THEN dtDia END) AS qtdeAtivacao28,
+    count(DISTINCT CASE WHEN dtDia >= date('2026-02-26', '-56 day') THEN dtDia END) AS qtdeAtivacao56
+FROM tb_transacoes
+GROUP BY IdCliente
