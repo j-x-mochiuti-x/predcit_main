4ff81b7d2eb8bbf28dc5ea864729bdda773eb4ad
j-x-mochiuti-x <joaomochi@gmail.com>
2026-03-09 12:46:29 -0300

    calculando saldo de pontos (positivo e negativo)

--- src/analytics/fs_transacional.sql
+++ src/analytics/fs_transacional.sql
@@ -16,6 +16,24 @@ SELECT IdCliente,
     count(DISTINCT CASE WHEN dtDia >= date('2026-02-26', '-7 day') THEN IdTransacao END) AS qtdeTransacao7,
     count(DISTINCT CASE WHEN dtDia >= date('2026-02-26', '-14 day') THEN IdTransacao END) AS qtdeTransacao14,
     count(DISTINCT CASE WHEN dtDia >= date('2026-02-26', '-28 day') THEN IdTransacao END) AS qtdeTransacao28,
-    count(DISTINCT CASE WHEN dtDia >= date('2026-02-26', '-56 day') THEN IdTransacao END) AS qtdeTransacao56
+    count(DISTINCT CASE WHEN dtDia >= date('2026-02-26', '-56 day') THEN IdTransacao END) AS qtdeTransacao56,
+    
+    sum(qtdePontos) AS saldoVida,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-7 day') THEN qtdePontos ELSE 0 END) AS saldos7,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-14 day') THEN qtdePontos ELSE 0 END) AS saldos14,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-28 day') THEN qtdePontos ELSE 0 END) AS saldos28,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-56 day') THEN qtdePontos ELSE 0 END) AS saldos56,
+
+    sum(CASE WHEN qtdePontos > 0 THEN qtdePontos ELSE 0 END) AS qtdePontosPosVida,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-7 day') AND qtdePontos > 0 THEN qtdePontos ELSE 0 END) AS qtdePontosPos7,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-14 day') AND qtdePontos > 0 THEN qtdePontos ELSE 0 END) AS qtdePontosPos14,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-28 day') AND qtdePontos > 0 THEN qtdePontos ELSE 0 END) AS qtdePontosPos28,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-56 day') AND qtdePontos > 0 THEN qtdePontos ELSE 0 END) AS qtdePontosPos56,
+
+    sum(CASE WHEN qtdePontos < 0 THEN qtdePontos ELSE 0 END) AS qtdePontosNegaVida,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-7 day') AND qtdePontos < 0 THEN qtdePontos ELSE 0 END) AS qtdePontosNega7,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-14 day') AND qtdePontos < 0 THEN qtdePontos ELSE 0 END) AS qtdePontosNega14,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-28 day') AND qtdePontos < 0 THEN qtdePontos ELSE 0 END) AS qtdePontosNega28,
+    sum(CASE WHEN dtDia >= date('2026-02-26', '-56 day') AND qtdePontos < 0 THEN qtdePontos ELSE 0 END) AS qtdePontosNega56
 FROM tb_transacoes
 GROUP BY IdCliente
