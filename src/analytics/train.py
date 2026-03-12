974928faa347d7903567d0b3179d6ce8b91d76a5
j-x-mochiuti-x <joaomochi@gmail.com>
2026-03-12 17:18:01 -0300

    modelahem treino e teste para ML

--- src/analytics/train.py
+++ src/analytics/train.py
@@ -18,3 +18,25 @@ df.head()
 # SAMPLE (Amostragem) - OOT 
 df_oot = df[df['dtRef']==df['dtRef'].max()].reset_index(drop=True)
 df_oot
+
+#%%
+# SAMPLE (Amostragem) - Treino e teste
+target = 'flFiel'
+features = df.columnstolist()[3:]
+
+df_train_test = df[df['dtRef']<df['dtRef'].max()].reset_index(drop=True)
+
+y= df_train_test[target]
+X= df_train_test[features]
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    X,y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y,
+)
+
+print(f"BASE TREINO: {y_train.shape[0]} Unidades  /\ Tx. target {100*y_train.mean():.2f}%")
+print(f"BASE TREINO: {y_test.shape[0]} Unidades  /\ Tx. target {100*y_test.mean():.2f}%")
+
+#%%
