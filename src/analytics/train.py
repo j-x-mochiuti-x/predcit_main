0e087f58165f9c0185ed5019e5caf42e68f5ee37
j-x-mochiuti-x <joaomochi@gmail.com>
2026-03-12 16:59:03 -0300

    importando dados

--- src/analytics/train.py
+++ src/analytics/train.py
@@ -8,3 +8,10 @@ from feature_engine import imputation
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 con = sqlalchemy.create_engine("sqlite:///../../data/analytics/database.db")
+
+#%%
+# SAMPLE (Amostragem) - Importando dados
+df = pd.read_sql("SELECT * FROM abt_fiel", con)
+df.head()
+
+#%%
